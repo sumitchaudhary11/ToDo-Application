ab71374f3715e29ad3625d0fb93607bf3e7a6b15
Sumit Kumar <49060256+sumitchaudhary11@users.noreply.github.com>
2024-03-02 08:36:20 +0530

    Files

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Todo from './Components/Todo';
+
+const App = () => {
+  
+  return (
+    <>
+    <h1 className='text-center'> To-Do Application </h1>
+    <Todo/>
+    
+
+    </>
+  );
+}
+
+export default App;
